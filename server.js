1293ae5d7d6a8c9cb4133d489e12c1aab61a064c
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-11-04 01:33:25 -0400

    continued fleshing out file structure and building onto the server.js file. Getting an error that app.use requires middleware

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const routes = require('./routes');
-const connection = require('./config/connection')
-
-
-const force = JSON.parse(process.env.DB_FORCE_SYNC);//if not working, change to object
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
