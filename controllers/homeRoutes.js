573538d2df2c0ebe85bf08fa07c3cb1cfbc11828
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-12-01 23:04:56 -0500

    building out server, deleted package-lock.json to remove from repo

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const withAuth = require('../utils/auth')
-const { User, Review, City } = require('../models')
+// const withAuth = require('../utils/auth')
+// const { User, Review, City } = require('../models')
 
 
 router.get('/', async (req, res) => {
